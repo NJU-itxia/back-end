596b64c0da7521bd1c3e492b89d879311cab2035
harrispino <2214102327@qq.com>
2016-10-27 23:19:09 +0800

    add  the authentication and authorization, change the data structure into three tags(collections) as client server and order. The order contain the _id of client and server which is able to qury.

--- njuitxia/API/api.py
+++ njuitxia/API/api.py
@@ -1,34 +1,44 @@
-# -*- coding: utf-8 -*-
-import json
-from pprint import pprint
-
+import bcrypt
 from eve import Eve
+from eve_swagger import swagger, add_documentation
 from eve.auth import BasicAuth
-from werkzeug.security import check_password_hash
 
-app = Eve()
 
-class RolesAuth(BasicAuth):
-     def check_auth(self, username, password, allowed_roles, resource, method):
-         # use Eve's own db driver; no additional connections/resources are used
-         accounts = app.data.driver.db['accounts']
-         lookup = {'username': username}
-         if allowed_roles:
-             # only retrieve a user if his roles match ``allowed_roles``
-             lookup['roles'] = {'$in': allowed_roles}
-         account = accounts.find_one(lookup)
-         return account and check_password_hash(account['password'], password)
+class BCryptAuth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource, method):
+        # use Eve's own db driver; no additional connections/resources are used
+        servers = app.data.driver.db['servers']
+        server = servers.find_one({'username': username})
+        # set 'auth_field' value to the server's username
+        if server and 'username' in server:
+            if server['role'] == 'admin':
+                print 'admin log'
+                return server['password'] == password
+            else:
+                print 'itxia log'
+                self.set_request_auth_value(server['username'])
+        return server and server['password'] == password
 
-def post_get_callback(resource, request, payload):
-    pprint(json.loads(payload.data))
+app = Eve(auth=BCryptAuth)
+app.register_blueprint(swagger)
 
-def post_post_callback(resource, request, payload):
-    pprint(json.loads(payload.data))
-    pprint(json.loads(request.data))
+app.config['SWAGGER_INFO'] = {
+    'title': 'itxia-backend',
+    'version': '1.0',
+    'description': 'an API description',
+    'termsOfService': 'my terms of service',
+    'contact': {
+        'name': 'chenhao',
+        'url': 'https://github.com/imphoney'
+    },
+    'license': {
+        'name': 'BSD',
+        'url': 'https://github.com/nicolaiarocci/eve-swagger/blob/master/LICENSE',
+    }
+}
 
-app.on_post_GET += post_get_callback
-app.on_post_POST += post_post_callback
+app.config['SWAGGER_HOST'] = '127.0.0.1:5000'
 
-if __name__ == '__main__':
-    app = Eve(auth=RolesAuth)
-    app.run(debug=True, host='0.0.0.0')
+
+if __name__ == '__main__':  
+    app.run()
